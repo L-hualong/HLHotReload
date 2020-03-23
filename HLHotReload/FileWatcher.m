efcb089fa19756f5fbbcd4fe8a38b47015538900
liuhualong <liuhualong.marco@bytedance.com>
2020-03-23 12:17:48 +0800

    添加注释

--- HLHotReload/FileWatcher.m
+++ HLHotReload/FileWatcher.m
@@ -40,13 +40,17 @@ static void fileCallback(ConstFSEventStreamRef streamRef,
         self.callback = callback;
         static struct FSEventStreamContext context;
         context.info = (__bridge void *)self;
-        fileEvents = FSEventStreamCreate(kCFAllocatorDefault,
-                                         fileCallback, &context,
-                                         (__bridge CFArrayRef) @[ projectRoot ],
-                                         kFSEventStreamEventIdSinceNow, .1,
-                                         kFSEventStreamCreateFlagUseCFTypes |
+        fileEvents = FSEventStreamCreate(kCFAllocatorDefault,//要用于为流分配内存的CFAllocator。传递NULL或kCFAllocatorDefault以使用当前默认分配器。
+                                         fileCallback,//一个FSEventStreamCallback，当FS事件发生时将调用它。
+                                         &context,//指向客户端希望与此流关联的FSEventStreamContext结构的指针
+                                         (__bridge CFArrayRef) @[ projectRoot ],//CFStringRefs的CFArray，每个CFArray指定一个目录的路径，表示要监视文件系统层次结构的根以进行修改。
+                                         kFSEventStreamEventIdSinceNow,//传入常量kFSEventStreamEventIdSinceNow，表示“从现在开始”要请求事件。
+                                         .1,//服务在从内核听到事件后应该等待的秒数
+                                         kFSEventStreamCreateFlagUseCFTypes |//修改正在创建的流的行为的标志
                                          kFSEventStreamCreateFlagFileEvents);
+        //调用者负责确保流被调度在至少一个RunLoop上，并且被调度的流正在至少一个RunLoop上运行。
         FSEventStreamScheduleWithRunLoop(fileEvents, CFRunLoopGetMain(), kCFRunLoopDefaultMode);
+        //尝试向FS Events服务注册，以便根据流中的参数接收事件。即开始监控
         FSEventStreamStart(fileEvents);
     }
 
